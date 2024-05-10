9ca7277bd3f3bb5d2e17e8c0caa0b970898a40d2
ASBcep <asaboritb2324@politecnics.barcelona>
2024-05-10 19:08:55 +0200

    Creo activities i classes (en blanc)

---
+++ app/src/main/java/asb/m08/UF2_1_1/AplicacioMultimediaAlexS/Objectes/Llistat_Arxius.kt
@@ -0,0 +1,12 @@
+package asb.m08.UF2_1_1.AplicacioMultimediaAlexS.Objectes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import asb.m08.UF2_1_1.AplicacioMultimediaAlexS.R
+
+class Llistat_Arxius : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_llistat_arxius)
+    }
+}
