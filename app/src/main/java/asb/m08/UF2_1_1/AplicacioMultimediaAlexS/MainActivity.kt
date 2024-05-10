cabff19a348bdeafe48c0ea6733a98efa490af87
ASBcep <asaboritb2324@politecnics.barcelona>
2024-05-10 16:38:46 +0200

    declaro views a MainActivity.kt

--- app/src/main/java/asb/m08/UF2_1_1/AplicacioMultimediaAlexS/MainActivity.kt
+++ app/src/main/java/asb/m08/UF2_1_1/AplicacioMultimediaAlexS/MainActivity.kt
@@ -2,10 +2,19 @@ package asb.m08.UF2_1_1.AplicacioMultimediaAlexS
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCrearTxt = findViewById<Button>(R.id.btnCrearTxt)
+        val btnCapturarFoto = findViewById<Button>(R.id.btnCapturarFoto)
+        val btnCapturarVideo = findViewById<Button>(R.id.btnCapturarVideo)
+        val btnCapturarSo = findViewById<Button>(R.id.btnCapturarSo)
+        val btnVisualitzarReproduir = findViewById<Button>(R.id.btnVisualitzarReproduir)
+        val btnServeisOnline = findViewById<Button>(R.id.btnServeisOnline)
+
     }
 }
--- app/src/main/java/asb/m08/UF2_1_1/AplicacioMultimediaAlexS/Objectes/Permanent.kt
+++ app/src/main/java/asb/m08/UF2_1_1/AplicacioMultimediaAlexS/Objectes/Permanent.kt
@@ -1,4 +1,5 @@
 package asb.m08.UF2_1_1.AplicacioMultimediaAlexS.Objectes
 
 object Permanent {
+
 }
